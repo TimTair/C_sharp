2d71c237e404a7acde04cd6fc242b6f20a560aff
Timur <tair.tim@yandex.ru>
2023-02-12 23:14:34 -0500

    Task07_save

---
+++ task07/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает 
+// трехзначное число и на выходе показывает 
+// последнюю цифру этого числа. 
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int LastNumber = number % 10;
+    Console.WriteLine(LastNumber);
+}
+else
+Console.WriteLine("Ошибка ввода");
